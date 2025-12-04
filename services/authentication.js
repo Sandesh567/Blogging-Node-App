2a9088fffe47457cd818cf5cda3386c8503cf97a
Sandesh567 <sandeshbhatta218@gmail.com>
2025-12-04 10:58:11 +0545

    configuring auth and adding env.

--- services/authentication.js
+++ services/authentication.js
@@ -1,7 +1,7 @@
 const JWT = require("jsonwebtoken");
+require('dotenv').config();
 
-const secret = "blog@123";
-
+const secret = process.env.SECRET;
 function createTokenForUser(user) {
   const payload = {
     _id: user._id,
